4da47017c95ad55b3bc8acbc7c4a028fb66e369e
pasankg <pasankg@gmail.com>
2024-08-18 17:34:24 +1000

    Create left, right components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import SplitScreen from "./components/split-screen.jsx";
 
-const LeftSdeComponent = () => {
-  return <h2 style={{ backgroundColor: "red" }}>I'm Left</h2>;
+const LeftSdeComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "red" }}>{title}</h2>;
 };
 
-const RightSideComponent = () => {
-  return <h2 style={{ backgroundColor: "green" }}>I'm Right</h2>;
+const RightSideComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "green" }}>{title}</h2>;
 };
 
 function App() {
   return (
-    <SplitScreen Left={LeftSdeComponent} Right={RightSideComponent} leftWidth = {1} rightWidth = {2}/>    
+    // version 1
+    // <SplitScreen Left={LeftSdeComponent} Right={RightSideComponent} leftWidth = {1} rightWidth = {2}/>
+
+    // version 2
+    <SplitScreen leftWidth={1} rightWidth={2}>
+      <LeftSdeComponent title={`I'm Left Side`} />
+      <RightSideComponent title={`I'm Right Side`} />
+    </SplitScreen>
   );
 }
 
--- src/components/split-screen.jsx
+++ src/components/split-screen.jsx
@@ -8,15 +8,28 @@ const Panel = styled.div`
   flex: ${(props) => props.flex};
 `;
 
-const SplitScreen = ({ Left, Right, leftWidth = 1, rightWidth = 1 }) => {
+// version 1
+
+// const SplitScreen = ({ Left, Right, leftWidth = 1, rightWidth = 1 }) => {
+//   return (
+//     <Container>
+//       <Panel flex={leftWidth}>
+//         <Left />
+//       </Panel>
+//       <Panel flex={rightWidth}>
+//         <Right />
+//       </Panel>
+//     </Container>
+//   );
+// };
+
+// version 2
+const SplitScreen = ({ children, leftWidth = 1, rightWidth = 1 }) => {
+  const [left, right] = children;
   return (
     <Container>
-      <Panel flex={leftWidth}>
-        <Left />
-      </Panel>
-      <Panel flex={rightWidth}>
-        <Right />
-      </Panel>
+      <Panel flex={leftWidth}>{left}</Panel>
+      <Panel flex={rightWidth}>{right}</Panel>
     </Container>
   );
 };
