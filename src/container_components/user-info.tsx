8cd04249a3169b7eba1ffdcf43adb3d34fd5170c
Ranidu Nadeesha <ranidunadeesha@gmail.com>
2024-08-27 22:20:13 +0800

    chore: refactor code

--- src/container_components/user-info.tsx
+++ src/container_components/user-info.tsx
@@ -1,13 +1,36 @@
-export const UserInfo = ({ user }) => {
-  const [name, age, country, books] = user || [];
+import { FC } from 'react'
+
+interface Address {
+  address: string
+  city: string
+  country: string
+}
+interface Book {
+  name: string
+  pages: number
+  title: string
+  price: number
+}
+export interface User {
+  firstName?: string
+  lastName?: string
+  age?: number
+  country?: string
+  books?: Book[]
+  address?: Address
+}
+
+export const UserInfo:FC<User> = (props) => {
+  if(!props) return null
+  const { firstName, lastName, age, address, books } = props
   return (
     <>
-      <h1>Name `{name}`</h1>
-      <p>Age `{age}`</p>
-      <p>Country: `{country}`</p>
+      <h1>{`Name ${firstName || '-'} ${lastName || '-'}`}</h1>
+      <p>{`Age ${age || '-'}`}</p>
+      <p>{`Country: ${address?.country || '-'}`}</p>
       <ul>
-        {books.map((book) => {
-          <li key={book}>{book}</li>;
+        {books?.map((book) => {
+          return <li key={`${book?.name}-${book?.title}`}>{book?.title}</li>;
         })}
       </ul>
     </>
